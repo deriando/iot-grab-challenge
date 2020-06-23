756daebf8a130d99f39b87b963a1652ae88f2a53
deriando <deriandogoh@gmail.com>
2020-06-24 04:43:09 +0800

    modified to work with directory restructuring

--- run_web_server.bat
+++ run_web_server.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
 
-cmd.exe /K "conda activate iot_env & python.exe web_server.py 80"
+cmd.exe /K "conda activate iot_env & python.exe webpage/webserver.py"
 
-rem http://localhost
+rem http://127.0.0.1:5000/
