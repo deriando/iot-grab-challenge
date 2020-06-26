6553f7d5d176e2024e83fa57dcaf018231ffbcd7
deriando <deriandogoh@gmail.com>
2020-06-27 05:16:03 +0800

    removed unused tokens

--- notifications.py
+++ notifications.py
@@ -2,8 +2,7 @@ import apprise
 
 notif = apprise.Apprise()
 
-#notif.add("mailto://deriando21:mdzbhaaqgzldxcfz@gmail.com")
-#notif.add("twilio://AC7dc8922d0fcba2a987b37b830d337e88:ecff7d681e29f98abe47473f90fc032a@1 678 515 9700/65 81002510") #not working?
+#notif.add("mailto://email:password@gmail.com")
 
 def telegramNotif(title, body):
     notif.add("tgram://1166066903:AAEfKq6lq5RkOmffXsSpkOzEyJ-KKzKYAGE")
